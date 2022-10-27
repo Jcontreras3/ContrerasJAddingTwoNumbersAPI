2ef36efda46215c1e9af25d8c6eb7ae791f5a57b
Jovann Contreras <jcontreras@codestack.co>
2022-10-27 13:08:49 -0700

    github

---
+++ Controllers/AddingController.cs
@@ -0,0 +1,28 @@
+//Jovann Contreras
+//Date 10/25/2022
+//Peer reviewed by: Freddy Aguilar - The program takes their mini challenge 2 and converts it to API format. It takes two number inputs from the user and adds them together, outputting them onto Postman. It works as intended and the code looks clean. Good job!
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ContrerasJAddingTwoNumbersAPI.Controllers
+{
+    [Route("[controller]")]
+    public class AddingController : Controller
+    {
+        [HttpGet]
+        [Route("Add/{num1}/{num2}")]
+
+        public int AddingNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
