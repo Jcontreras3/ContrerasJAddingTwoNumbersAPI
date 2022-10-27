e8b1839cf2739c5adbb5a283247f1843df8f195e
Jovann Contreras <jcontreras@codestack.co>
2022-10-27 13:41:41 -0700

    u

--- Controllers/AddingController.cs
+++ Controllers/AddingController.cs
@@ -3,7 +3,6 @@
 //Peer reviewed by: Freddy Aguilar - The program takes their mini challenge 2 and converts it to API format. It takes two number inputs from the user and adds them together, outputting them onto Postman. It works as intended and the code looks clean. Good job!
 
 
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
